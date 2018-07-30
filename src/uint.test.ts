d25e3b47a0e7ffce00c9e8082d11c60dadddb6a0
Timothy Farkas <tim@streamsimple.io>
2018-07-29 17:08:06 -0700

    - Added more tests for isUInt

--- src/uint.test.ts
+++ src/uint.test.ts
@@ -5,8 +5,16 @@ const expect = chai.expect;
 
 describe('UInt', () => {
   describe('isInteger tests', () => {
-    it('should say the number is an integer.', () => {
+    it('should say the number is an unsigned integer.', () => {
       expect(isUInt(0x8FFFFFFF)).to.be.true;
+    });
+
+    it('should say the number is not an unsigned integer.', () => {
+      expect(isUInt(-1)).to.be.false;
+    });
+
+    it('should say that doubles are not unsigned integers.', () => {
+      expect(isUInt(1.1)).to.be.false;
     });
   });
 });
